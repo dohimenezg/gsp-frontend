1d0e7afb9d0c7bb25d78e15b75e27e2de4f80fa1
dohimenezg <djimenezg@unicauca.edu.co>
2022-12-04 00:31:59 -0500

    Pantalla de Incio estática

--- pages/inicio.js
+++ pages/inicio.js
@@ -2,7 +2,8 @@ import { FiHome, FiCompass } from 'react-icons/fi';
 import { HiOutlineDocumentPlus } from 'react-icons/hi2';
 import { BsGear } from 'react-icons/bs'
 import Sidebar from '../components/sidebar';
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text, Image } from '@chakra-ui/react';
+import StickyBox from 'react-sticky-box';
 
 export default function Inicio () {
     const pages_to_go = [
@@ -28,12 +29,12 @@ export default function Inicio () {
         }
     ]
     return (
-        <Flex>
-            <Sidebar options={pages_to_go} />
-            <Box>
-                <Text>
-                    Hola mundo
-                </Text>
+        <Flex display="flex" alignItems="flex-start">
+            <StickyBox >
+                <Sidebar options={pages_to_go}/>
+            </StickyBox>
+            <Box minHeight="100vh" minWidth="100vh" bgColor="rgb(20, 20, 30)" w="100%">
+                <Image src='inicio.svg'></Image>
             </Box>
         </Flex>
     )
