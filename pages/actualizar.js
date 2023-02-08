f825e3fcb15d5ed2ffe6d5135b297e46275d5e5c
dohimenezg <djimenezg@unicauca.edu.co>
2023-02-07 20:13:53 -0500

    Se repara el uso de parametros obtenidos desde una url manualmente ingresada

--- pages/actualizar.js
+++ pages/actualizar.js
@@ -3,16 +3,36 @@ import StickyBox from 'react-sticky-box'
 import ActualizarTramite from '../components/layouts/actualizar-tramite'
 import MainSidebar from '../components/layouts/main-sidebar'
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 
 export default function Actualizar() {
   const router = useRouter()
-  const { id_tramite } = router.query
+  const [idTramite, setIdTramite] = useState(null)
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setIdTramite(router.query.id_tramite)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router])
+
+  useEffect(() => {
+    setIdTramite(router.query.id_tramite)
+  }, [router.query.id_tramite])
+
+  if (!idTramite) {
+    return null
+  }
+
   return (
     <Flex display="flex" alignItems="flex-start">
       <StickyBox>
         <MainSidebar />
       </StickyBox>
-      <ActualizarTramite id_tramite={id_tramite} />
+      <ActualizarTramite id_tramite={idTramite} />
     </Flex>
   )
 }
